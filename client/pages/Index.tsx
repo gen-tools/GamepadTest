63fc0a3fc5abb38d84be5c951add9a6e04d4491c
gen-tools <shahazaibseo038@gmail.com>
2026-02-06 16:44:04 +0500

    Update Index.tsx

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -353,7 +353,11 @@ export default function Index() {
           </div>
         </div>
       </section>
+      {/* Second Ezoic Ad with unique ID */}
+        <div id="ezoic-pub-ad-placeholder-102" className="my-8"></div>
 
+        <AdsterraAd />
+        <RecommendedProductsSection products={recommendedProducts} />
       {/* New Content Section */}
       <section className="py-20 sm:py-32 bg-muted/50">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
